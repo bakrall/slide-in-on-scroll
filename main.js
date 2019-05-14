372205df44e95612d070644df4824bacd26baef1
Barbara Stadnik <barbara1stadnik@gmail.com>
2019-05-14 20:16:37 +0200

    add debounce function
    to reduce number of times when scroll event is fired

--- main.js
+++ main.js
@@ -1,7 +1,24 @@
 const sliderImages = document.querySelectorAll('.slide-in');
 
+function debounce(func, wait = 20, immediate = true) {
+  let timeout;
+
+  return function() {
+    const context = this, args = arguments;
+    const later = function() {
+      timeout = null;
+      if(!immediate) func.apply(context, args);
+    };
+
+    const callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  }
+}
+
 function checkSlide(e) {
   console.count(e);
 }
 
-window.addEventListener('scroll', checkSlide);
+window.addEventListener('scroll', debounce(checkSlide));
